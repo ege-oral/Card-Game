6a6a6e4c9e8e9e3c50d1d4c137cc4e09a95ca0a8
Ege Oral <egeoral@gmail.com>
2025-02-17 16:14:57 +0300

    Zenject added

---
+++ Assets/Scripts/Scenes/SceneLoaderController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace SceneLoader
+{
+    public class SceneLoaderController : MonoBehaviour
+    {
+        [SerializeField] private bool playTransitionOnStart;
+        
+        
+    }
+}
