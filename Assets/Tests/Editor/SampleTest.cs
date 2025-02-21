0939237b0b4bb6722e4b024249a78b0ecf71db33
Ege Oral <egeoral@gmail.com>
2025-02-21 22:37:58 +0300

    Custom assemblies created and Moq package added

---
+++ Assets/Tests/Editor/SampleTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace Tests.Editor
+{
+    [TestFixture]
+    public class SampleTest
+    {
+        [Test]
+        public void TestExample()
+        {
+            Assert.AreEqual(2, 1 + 1);
+        }
+    }
+}
